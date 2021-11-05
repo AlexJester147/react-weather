8d48334d00b59568cf7aacdb452353fe12c343d4
AlexJester147 <vardy2016@lenta.ru>
2021-11-05 11:19:28 +0300

    feat : add weather info

--- src/App.js
+++ src/App.js
@@ -12,11 +12,16 @@ class App extends React.Component{
 temp:undefined,
 city:undefined,
 weather:undefined,
+wind:undefined,
+lat:undefined,
+lon:undefined,
 country:undefined,
 pressure:undefined,
 error:undefined,
 message:undefined
   }
+
+
 
   gettingWeather = async(e) => {
     e.preventDefault();
@@ -26,18 +31,30 @@ message:undefined
 
     const API_URL = await fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&lang=ru&appid=' + API_KEY +'&units=metric');
     const data = await API_URL.json()
-
+console.log(data)
 if (data.name){
     this.setState({
       temp:Math.ceil(data.main.temp),
       city:data.name,
       weather:data.weather[0].description,
+      wind:Math.ceil(data.wind.speed),
+      lat:data.coord.lat.toFixed(2),
+      lon:data.coord.lon.toFixed(2),
       country:data.sys.country,
       pressure:data.main.pressure,
       error:undefined,
       img:data.weather[0].icon,
       message:data.message
     })
+
+    let url1 = `https://api.unsplash.com/photos/random?orientation=landscape&query=${data.weather[0].main}&client_id=u5GkLdG0m9Izf8GiqLuqu9TctrIWVMZFemY2FP_Sq4A`;
+    const res1 = await fetch(url1);
+    const data1 = await res1.json();
+    if (data1.urls.regular){
+      document.body.style.backgroundImage = `url(${data1.urls.regular})`;
+    }
+    
+    console.log(data1)
 }
 
 else if (data.message){
@@ -45,6 +62,9 @@ else if (data.message){
     temp:undefined,
     city:undefined,
     weather:undefined,
+    wind:undefined,
+    lat:undefined,
+    lon:undefined,
     country:undefined,
     pressure:undefined,
     error:undefined,
@@ -58,12 +78,19 @@ else if (data.message){
       temp:undefined,
       city:undefined,
       weather:undefined,
+      wind:undefined,
+      lat:undefined,
+      lon:undefined,
       country:undefined,
       pressure:undefined,
       error:'Введите название города',
       message:undefined
     })
   }
+
+
+
+
   }
   
 
@@ -77,6 +104,9 @@ else if (data.message){
         temp={this.state.temp}
         city={this.state.city}
         weather={this.state.weather}
+        wind={this.state.wind}
+        lat={this.state.lat}
+        lon={this.state.lon}
         country={this.state.country}
         pressure={this.state.pressure}
         error={this.state.error}
